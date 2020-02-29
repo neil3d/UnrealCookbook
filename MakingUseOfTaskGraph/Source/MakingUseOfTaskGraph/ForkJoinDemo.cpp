7df378501b291298e767d05de2764f7f908a710d
neil3d <neil3d@126.com>
2020-02-29 11:12:20 +0800

    begin of Making use of TaskGraph

---
+++ MakingUseOfTaskGraph/Source/MakingUseOfTaskGraph/ForkJoinDemo.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ForkJoinDemo.h"
+
+// Sets default values
+AForkJoinDemo::AForkJoinDemo()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AForkJoinDemo::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AForkJoinDemo::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
