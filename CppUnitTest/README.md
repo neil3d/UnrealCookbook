d2e2e23009e06be789f71f37bdc28df266409da2
neil3d <neil3d@126.com>
2020-07-03 21:01:55 +0800

    create unit test project

---
+++ CppUnitTest/README.md
@@ -0,0 +1,5 @@
+# UE4 环境下的 C++ 单元测试
+
+## Further Reading
+
+- [Automation System Overview](https://docs.unrealengine.com/en-US/Programming/Automation/index.html)
---
+++ CppUnitTest/Source/CppUnitTest/CppUnitTest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CppUnitTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CppUnitTest, "CppUnitTest" );
