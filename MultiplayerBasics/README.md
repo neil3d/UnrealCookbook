47d19590818ba106015265ef64de847b92f9f080
neilfang <neilfang@tencent.com>
2022-06-23 07:33:34 +0800

    basic 3rd person character setup

--- MultiplayerBasics/README.md
+++ MultiplayerBasics/README.md
@@ -15,3 +15,20 @@
   - C++: void AActor::SetReplicates(bool bInReplicates)
   - 蓝图 ClassDefaults:
     - [x] Replication|Replicates
+- 创建和销毁的操作(Spawn/Destroy)需要在Server端执行，才会同步到各个Client
+  - 在Client执行 Spawn/Destroy 不会被同步
+  - When you spawn or destroy a replicated Actor, ensure that you do it on the server.
+
+## 【实验3】数据类型变量同步|Variable Replication
+
+- RepNotify
+
+## 【实验4】Object 引用同步
+
+- [官方文档：Replicating object references](https://docs.unrealengine.com/5.0/en-US/replicating-object-references-in-unreal-engine/)
+
+## 【实验5】RPCs
+
+- [官方文档：RPCs](https://docs.unrealengine.com/5.0/en-US/rpcs-in-unreal-engine/)
+
+## 【实验6】移动同步|Movement Replication
---
+++ MultiplayerBasics/Source/MultiplayerBasics/MyCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCharacter.h"
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
