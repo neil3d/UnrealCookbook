ef3e9f88d02f556aaab88e6a31341ed3c307971c
neilfang <neilfang@tencent.com>
2022-06-23 08:07:42 +0800

    基本的属性同步

--- MultiplayerBasics/Source/MultiplayerBasics/MyCharacter.cpp
+++ MultiplayerBasics/Source/MultiplayerBasics/MyCharacter.cpp
@@ -1,21 +1,37 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "MyCharacter.h"
+#include "Net/UnrealNetwork.h"
 
 // Sets default values
 AMyCharacter::AMyCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// 开启 Replication
+	bReplicates = true;
+}
+
+void AMyCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AMyCharacter, CurrentHP);
 }
 
 // Called when the game starts or when spawned
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// 服务端逻辑：设置 Timer 修改 CurrentHP
+	if (GetLocalRole() == ROLE_Authority)
+	{
+		UWorld* WorldContext = GetWorld();
+		WorldContext->GetTimerManager().SetTimer(TestTimer, this, &AMyCharacter::OnTimer, 2.0, true);
+	}
 }
 
 // Called every frame
@@ -25,10 +41,13 @@ void AMyCharacter::Tick(float DeltaTime)
 
 }
 
-// Called to bind functionality to input
-void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AMyCharacter::OnRep_CurrentHP()
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	ReciveCurrentHPChanged();
 }
 
+void AMyCharacter::OnTimer()
+{
+	check(GetLocalRole() == ROLE_Authority);
+	CurrentHP += 5;
+}
--- MultiplayerBasics/Source/MultiplayerBasics/MyCharacter.h
+++ MultiplayerBasics/Source/MultiplayerBasics/MyCharacter.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -15,6 +15,9 @@ public:
 	// Sets default values for this character's properties
 	AMyCharacter();
 
+	// Replication 属性同步必要的重载函数
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -23,7 +26,18 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+	// 定义一个具备 Rep Notify 的同步属性变量
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, ReplicatedUsing=OnRep_CurrentHP)
+	float CurrentHP;
 
+	// 用来接收 Rep Notify 的回调函数
+	UFUNCTION()
+	void OnRep_CurrentHP();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void ReciveCurrentHPChanged();
+
+	FTimerHandle TestTimer;
+	void OnTimer();
 };
